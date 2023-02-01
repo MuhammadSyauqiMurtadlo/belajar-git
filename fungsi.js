cb8e5130b483ef25b12ff198ef82ceca57badc4f
Syauqi Murtadlo <sauki084@gmail.com>
2023-02-01 10:49:58 +0700

    menghapus file yang tidak perlu

--- fungsi.js
+++ fungsi.js
@@ -5,11 +5,15 @@ console.log(add(1, 2));
 
 // ! SYNTAX PENTING UNTUK DIGUNAKAN DI TERMINAL :
 // Node (nama file)
-// git diff
-// git status
+// git diff = digunakan untuk mencari perbedaan dari versi sebelumnya
+// git status = untuk melihat status git saat ini itu seperti apa
 // git add . (jika tidak menggunakan titik (.), bisa langsung menyertakan nama file)
-// git commit -m ("")
+// git commit -m ("") = digunakan untuk meng--commit file yg sudah ada di staged changes
 // git log = untuk melihat perjalanan history
 // git restore = untuk membatalkan perubahan
-// git restore . = untuk membatalkan perubahan semuanya
+
+// git restore -S (nama file) = untuk mengembalikan file dari staged changes ke working directory
+// git reset = untuk mengembalikan file dari staged changes ke working directory secara keselirihan
+// git reset --hard (nomer commit) = untuk kembali ke commit sebelumnya
+
 // ? Belajar GIT
