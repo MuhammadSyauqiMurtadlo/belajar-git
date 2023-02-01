f4b7946d140c03e20e7c91805256c4c65ffa5ce8
Syauqi Murtadlo <sauki084@gmail.com>
2023-02-01 11:14:09 +0700

    menambahkan fitur perkalian

--- fungsi.js
+++ fungsi.js
@@ -3,6 +3,12 @@ console.log("Fungsi Pertama");
 const add = (a, b) => a + b;
 console.log(add(1, 2));
 
+const subtract = (a, b) => a - b;
+console.log(subtract(5, 2));
+
+const multiply = (a, b) => a * b;
+console.log(multiply(3, 2));
+
 // ! SYNTAX PENTING UNTUK DIGUNAKAN DI TERMINAL :
 // Node (nama file)
 // git diff = digunakan untuk mencari perbedaan dari versi sebelumnya
